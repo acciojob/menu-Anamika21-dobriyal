edac60f9698f311ad4ffb83313dfd253bc5e9d34
Anamika <anamikadobriyal21@gmail.com>
2025-01-23 23:55:54 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,8 @@ import React, { useState } from "react";
 import Menu from "./Menu";
 import Categories from "./Categories";
 import items from "./data";
+import './App.css';
+
 
 const allCategories = ["All", ...new Set(items.map((item) => item.category))];
 
